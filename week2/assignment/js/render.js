bac65481496302b3adef5d574eb95d93938131bf
gahyeon <gus_05@naver.com>
2024-10-28 00:53:57 +0900

    feat: GitHub 사용자 이름을 링크로 변경

--- week2/assignment/js/render.js
+++ week2/assignment/js/render.js
@@ -21,7 +21,7 @@ export const renderDatas = (membersData) => {
         <td><input type="checkbox" class="check-item" /></td>
         <td id="name">${name}</td>
         <td id="englishName">${englishName}</td>
-        <td id="github">${github}</td>
+        <td id="github"><a href="https://github.com/${github}" target="_blank">${github}</a></td>
         <td id="gender">${gender === "male" ? "남자" : "여자"}</td>
         <td id="role">${role}</td>
         <td id="firstWeekGroup">${firstWeekGroup}</td>
