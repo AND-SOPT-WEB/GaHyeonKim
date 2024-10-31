672afee0ace1f308bd8f89d1575476d347601946
gahyeon <gus_05@naver.com>
2024-10-31 16:39:14 +0900

    design: theme 세팅

--- week3/assignment/src/styles/theme.js
+++ week3/assignment/src/styles/theme.js
@@ -1,9 +1,10 @@
 import { css } from "@emotion/react";
 
 const colors = {
-        
+        lightGreen: '#E1F0E7',
+        midGreen: '#619584',
+        darkGreen: '#233641',
     };
-
 
 export const Theme = {
     colors
